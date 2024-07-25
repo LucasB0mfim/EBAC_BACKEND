c4d6ed5a1c6ce12e00e62f7bf8d9935a1a4c051b
lucas <lucasbomfimsobral@gmail.com>
2024-07-25 08:25:44 -0300

    initial commit

---
+++ src/main/java/com/mycompany/cadastroclienteswingnetbeans/CadastroClienteSwingNetBeans.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cadastroclienteswingnetbeans;
+
+/**
+ *
+ * @author Lucas
+ */
+public class CadastroClienteSwingNetBeans {
+
+    public static void main(String[] args) {
+        TelaPrincipal tela = new TelaPrincipal();
+        tela.setVisible(true);
+    }
+}
